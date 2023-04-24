f316994d1ccfb8ace6ccedfb817860657e7cf6ef
Nataliya Kim <magistr1982@gmail.com>
2023-04-24 14:53:45 +0300

    replace contacts array into new file

--- src/components/modules/Contacts/Contacts.jsx
+++ src/components/modules/Contacts/Contacts.jsx
@@ -4,15 +4,11 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ContactList from './ContactList/ContactList';
 import ContactFilter from './ContactFilter/ContactFilter';
 import css from './contacts.module.css';
+import contacts from './items';
 
 class Contacts extends Component {
   state = {
-    contacts: [
-      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [...contacts],
     name: '',
     number: '',
     filter: '',
